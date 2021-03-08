0a0b97d2621b979ef309cbe5968a0ae7735e9a92
Maiara Garcia <maiara.garcia@bevicred.com.br>
2021-03-08 15:20:29 -0300

    alterando os icons

--- lib/app/enums/weather-icon.enum.dart
+++ lib/app/enums/weather-icon.enum.dart
@@ -5,15 +5,15 @@ import 'package:mdi/mdi.dart';
 
 class WeatherIconEnum
 {
-  static const IconData snow = Mdi.snowflake;
+  static const IconData snow = Mdi.weatherSnowyHeavy;
   static const IconData sleet = Mdi.sleep;
-  static const IconData hail = Icons.hail;
-  static const IconData thunderstorm = Mdi.thumbDown;
-  static const IconData heavyRain = Mdi.weatherSnowyHeavy;
-  static const IconData lightRain = Icons.lightbulb;
-  static const IconData showers = Icons.show_chart_rounded;
-  static const IconData heavyCloud = Icons.cloud;
-  static const IconData lightCloud = Icons.cloud_upload_sharp;
-  static const IconData clear = Mdi.skype;
-  static const IconData unknown = Icons.device_unknown;
+  static const IconData hail = Mdi.weatherHail;
+  static const IconData thunderstorm = Mdi.weatherLightningRainy;
+  static const IconData heavyRain = Mdi.weatherPouring;
+  static const IconData lightRain = Mdi.weatherRainy;
+  static const IconData showers = Mdi.weatherPartlySnowyRainy;
+  static const IconData heavyCloud = Mdi.weatherCloudyAlert;
+  static const IconData lightCloud = Mdi.weatherPartlyCloudy;
+  static const IconData clear = Mdi.weatherSunny;
+  static const IconData unknown = Mdi.weatherSunnyOff;
 }
